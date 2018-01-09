30d8bbb0c891d13d1e937a7a9426c3aabae8d6f6
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-01-08 19:02:26 -0800

    create ProjectList component to display on root route

---
+++ src/components/ProjectList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <p>Project list displays.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ProjectList',
+  components: { },
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
+import ProjectList from '@/components/ProjectList';
 
 Vue.use(Router);
 
@@ -7,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: '',
-      component: ,
+      name: 'ProjectList',
+      component: ProjectList,
     },
   ],
 });
