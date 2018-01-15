b28100c8ccdd2ed949e08ab1e441c172524b61c0
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-01-14 21:46:36 -0800

    display projects copy and additional properties

--- src/components/ProjectItem.vue
+++ src/components/ProjectItem.vue
@@ -2,6 +2,7 @@
   <div>
     <h3>{{project.title}}</h3>
     <p>{{project.description}}</p>
+    <p><span v-for='technology in project.technologies'>{{technology}} </span><span v-for='language in project.languages'>{{language}} </span></p>
   </div>
 </template>
 
