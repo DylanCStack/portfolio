ec6110d12fe6607aca29f97543665bbd15cc079a
Dylan Stackhouse <Stackhouse.Dylan@gmail.com>
2018-01-20 16:55:55 -0800

    add background 'guideline'

--- src/App.vue
+++ src/App.vue
@@ -15,11 +15,26 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  border: 0;
+  padding: 0;
   margin: 0;
+  color: white;
+
+  background:
+    radial-gradient(circle closest-side at 50% 70%, #bbb 90%, black 91%, black 94%, transparent 95%),
+    radial-gradient(circle closest-side at 50% 30%, #bbb 90%, black 91%, black 94%, transparent 95%),
+    linear-gradient(#000, #000),
+    linear-gradient(#000, #000),
+    linear-gradient(#000 10%, #bbb 11%, #bbb 79%, #000 80%);
+  background-repeat: no-repeat, no-repeat, no-repeat, no-repeat, repeat-y;
+  background-position: 50% top, 50% bottom, top, bottom, 50% 30vh;
+  background-size: 60vh 60vh, 60vh 60vh, 100% 60vh, 100% 60vh, 4vh 15vh;
 }
 * {
   border:0;
   margin:0;
+}
+body{
+  background-color:black;
+  color:#aaa;
 }
 </style>
